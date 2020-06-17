4a9542ecd27e988cd38e03783bfea2685a2b181c
GillSam <57861280+GillSam@users.noreply.github.com>
2020-06-17 19:05:29 +0300

    Initial commit

---
+++ tempi/tempi.py
@@ -0,0 +1,24 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+''' IMPORTS '''
+
+import json
+
+# Disable insecure warnings
+requests.packages.urllib3.disable_warnings()
+
+''' CONSTANTS '''
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+
+def say_hello_command(client, args):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
